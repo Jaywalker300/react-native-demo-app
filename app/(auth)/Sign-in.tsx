b74c8277faf50cfc83888bb2713577030e49cd4b
oluwachike <chiketravis300@gmail.com>
2026-06-10 13:08:53 +0100

    first commit

---
+++ app/(auth)/Sign-in.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+const Signin = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text>Signin</Text>
+      <Link
+        href="/(auth)/Sign-in"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Log in
+      </Link>
+      link to home
+      <Link href="/" className=" mt-4 p-64: rounded bg-primary text-white p-4">
+        back
+      </Link>
+    </View>
+  );
+};
+
+export default Signin;
